c8959284b3944341183f6d8570f4a8ca8a77cd76
Kevin Hughes <kevinhughes27@gmail.com>
2017-11-27 20:42:36 -0500

    config fonts

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,6 +15,15 @@ module.exports = {
     },
     {
       resolve: 'gatsby-transformer-remark'
+    },
+    {
+      resolve: `gatsby-plugin-google-fonts`,
+      options: {
+        fonts: [
+          `Fanwood`,
+          `Quattrocento\:400,700`
+        ]
+      }
     }
   ]
 }
