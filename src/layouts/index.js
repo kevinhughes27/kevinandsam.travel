3e2bd1e878174da9e262340addba5688514cd8b7
Kevin Hughes <kevinhughes27@gmail.com>
2017-11-27 20:29:35 -0500

    add navigation

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,26 +1,54 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
+import GatsbyLink from 'gatsby-link'
 
 import '../styles/index.scss'
 
-const TemplateWrapper = ({ children }) => (
-  <div>
-    <Helmet
-      title="kevinandsam.travel"
-      meta={[
-        { name: 'description', content: 'Sample' },
-        { name: 'keywords', content: 'sample, something' },
-      ]}
-    />
-    <div>
+const Head = () => (
+  <Helmet
+    title="kevinandsam.travel"
+    meta={[
+      { name: 'description', content: 'Sample' },
+      { name: 'keywords', content: 'sample, something' },
+    ]}
+  />
+)
+
+const TopNav = () => (
+  <header className="header">
+    <a href="/">
+      <figure className="header__img">
+      </figure>
+    </a>
+    <nav>
+      <ul className="header__list">
+        <li>
+          <GatsbyLink to="/">Home</GatsbyLink>
+        </li>
+        <li>
+          <GatsbyLink to="/archive">About</GatsbyLink>
+        </li>
+        <li>
+          <GatsbyLink to="/archive">Blog</GatsbyLink>
+        </li>
+      </ul>
+    </nav>
+  </header>
+)
+
+const Layout = ({ children }) => (
+  <div id="home">
+    <Head />
+    <TopNav />
+    <main>
       {children()}
-    </div>
+    </main>
   </div>
 )
 
-TemplateWrapper.propTypes = {
+Layout.propTypes = {
   children: PropTypes.func,
 }
 
-export default TemplateWrapper
+export default Layout
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import GatsbyLink from 'gatsby-link';
 import { withPrefix } from 'gatsby-link'
 
 const IndexPage = () => (
@@ -7,7 +6,6 @@ const IndexPage = () => (
     <header className='mast__header'>
       <h1>Kevin & Sam</h1>
       <span>Eat. Sleep. Travel.</span>
-      <GatsbyLink to="/archive">Blog</GatsbyLink>
     </header>
     <div className='mast__img bg-alpha'>
       <div className='absolute-bg' style={{backgroundImage: `url('${__PATH_PREFIX__}/mast-left.jpg')`}}></div>
