13f85e2fbc424ad6c9885951feb7dfa4d0f857ba
Kevin Hughes <kevinhughes27@gmail.com>
2017-12-06 13:48:20 -0500

    about page copy

--- src/pages/about.js
+++ src/pages/about.js
@@ -9,7 +9,10 @@ const AboutPage = () => (
       <h1 className="section-header">About Us</h1>
       <div className="about-text">
         <p className="about__text wow fadeInUp" data-wow-delay="300ms">
-          Fixie quinoa hot chicken, 8-bit tousled jianbing portland yr truffaut beard. Dreamcatcher ethical brunch authentic kale chips. Hell of raclette tumblr, organic kinfolk locavore occupy gastropub pinterest. Ramps squid la croix, chicharrones gochujang fixie try-hard bitters.
+          Kevin and Sam met in 2014 through a mutual love of travelling. Their first adventure together was in 2015 to Costa Rica.
+          Since that first trip things have only escalated, hiking the Inca trail in Peru and backpacking around China for 4 weeks.
+          In 2018 Sam and Kevin are embarking on their most ambitious adventure yet -
+          we are selling all of our stuff and becoming nomads to see the world.
         </p>
       </div>
 
