ba17d23b2e529b671e343b02ea95ec02da9da2cc
Kevin Hughes <kevinhughes27@gmail.com>
2017-12-11 14:37:17 -0500

    change index page to a countdown timer

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,18 @@
 import React from 'react'
+import Countdown from 'react-cntdwn'
 
 const IndexPage = () => (
-  <div>
-    Coming Soon
+  <div id="countdown">
+    <Countdown
+      targetDate={new Date('9:10 AM Feb 1, 2018')}
+      format={{
+        day: 'dd days',
+        hour: 'hh hours',
+        minute: 'mm minutes',
+        second: 'and ss seconds until departure 🚀'
+      }}
+      timeSeparator={' '}
+    />
   </div>
 )
 
