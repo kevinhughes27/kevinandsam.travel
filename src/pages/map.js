21fab8da9e8a75ac20b2f7613d2c3ab8a63bd0a3
Kevin Hughes <kevinhughes27@gmail.com>
2018-01-03 15:07:52 -0500

    center map manually

--- src/pages/map.js
+++ src/pages/map.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react'
 import { Map, TileLayer, Circle, CircleMarker } from 'react-leaflet'
 
+const zoom = 3
+const center = [23.76, -34.27]
+
 const currentLocation = {
   name: 'Ottawa Canada',
   coordinates: [45.4215, -75.6972]
@@ -12,26 +15,29 @@ const route = [
   { name: "La Paz Bolivia", coordinates: [-16.499998, -68.1333328] },
 ]
 
+const tileProviderUrl =
+  'https://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Light_Gray_Base/MapServer/tile/{z}/{y}/{x}'
+
+const RouteMarkers = ({ locations }) => {
+  const markers = locations.map((location, i) => (
+    <Circle key={i} center={location.coordinates} radius={5} />
+  ))
+  return <div>{markers}</div>
+}
+
 class MapPage extends Component {
-  constructor(props) {
-    super(props)
-
-    this.state = {
-      center: currentLocation.coordinates,
-      zoom: 3,
-    }
-  }
-
   render() {
-    const {zoom, center} = this.state
-
     return (
-      <Map center={center} zoom={zoom} zoomControl={false}>
-        <TileLayer url='https://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Light_Gray_Base/MapServer/tile/{z}/{y}/{x}' />
-        <CircleMarker center={center} radius={5} />
-        {route.map((location, i) => (
-          <Circle key={i} center={location.coordinates} radius={5} />
-        ))}
+      <Map
+        center={center}
+        zoom={zoom}
+        zoomControl={false}
+        onDrag={(ev) => console.log(ev.target._lastCenter)}>
+
+        <TileLayer url={tileProviderUrl} />
+        <CircleMarker center={currentLocation.coordinates} radius={5} />
+        <RouteMarkers locations={route} />
+
       </Map>
     )
   }
