703b0abe15e2da6ac294f62c39bd1d24b04a16c6
Kevin Hughes <kevinhughes27@gmail.com>
2018-01-04 16:09:20 -0500

    clean up text-container styles. make them generic instead of coupling to about page

--- src/pages/visit.js
+++ src/pages/visit.js
@@ -4,6 +4,16 @@ class VisitPage extends Component {
   render () {
     return (
       <section id="visit" className="section-padding">
+        <div className="grid">
+          <div className="text-container">
+            <p className="wow fadeInUp" data-wow-delay="300ms">
+              Kevin and Sam met in 2014 through a mutual love of travelling. Their first adventure together was in 2015 to Costa Rica.
+              Since that first trip things have only escalated, hiking the Inca trail in Peru and backpacking around China for 4 weeks.
+              In 2018 Sam and Kevin are embarking on their most ambitious adventure yet -
+              we are selling all of our stuff and becoming nomads to see the world.
+            </p>
+          </div>
+        </div>
       </section>
     )
   }
