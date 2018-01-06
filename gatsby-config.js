030207f6b7cbed2713b020d21140404e12d6849d
Kevin Hughes <kevinhughes27@gmail.com>
2018-01-06 18:21:28 -0500

    add google analytics closes #3

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,27 +1,26 @@
-const files = [
+const blog = [
   {
     resolve: `gatsby-source-filesystem`,
     options: {
       path: `${__dirname}/blog`,
       name: 'pages',
     },
+  },
+  {
+    resolve: 'gatsby-transformer-remark',
+    options: {
+      plugins: [
+        'gatsby-remark-copy-linked-files',
+        {
+          resolve: 'gatsby-remark-images',
+          options: {
+            linkImagesToOriginal: false
+          }
+        },
+      ]
+    }
   }
 ]
-
-const markdownTransformer = {
-  resolve: 'gatsby-transformer-remark',
-  options: {
-    plugins: [
-      'gatsby-remark-copy-linked-files',
-      {
-        resolve: 'gatsby-remark-images',
-        options: {
-          linkImagesToOriginal: false
-        }
-      },
-    ]
-  }
-}
 
 const googleFonts = {
   resolve: `gatsby-plugin-google-fonts`,
@@ -31,6 +30,14 @@ const googleFonts = {
       `Quattrocento\:400,700`
     ]
   }
+}
+
+const googleAnalytics = {
+  resolve: `gatsby-plugin-google-analytics`,
+  options: {
+    trackingId: "UA-112059908-1",
+    anonymize: true
+  },
 }
 
 module.exports = {
@@ -44,8 +51,8 @@ module.exports = {
     `gatsby-plugin-sharp`,
     `gatsby-plugin-sass`,
     `gatsby-transformer-sharp`,
-    markdownTransformer,
     googleFonts,
-    ...files,
+    googleAnalytics,
+    ...blog,
   ]
 }
