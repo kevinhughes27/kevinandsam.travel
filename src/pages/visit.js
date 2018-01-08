884f10ede2be3b424533501d93b36c4e40b24dd4
Kevin Hughes <kevinhughes27@gmail.com>
2018-01-08 14:57:32 -0500

    use custom button to open calendar input

--- src/pages/visit.js
+++ src/pages/visit.js
@@ -82,6 +82,7 @@ class VisitPage extends Component {
 
   render () {
     const { when, where } = this.state
+
     const whereOptions = locations
       .filter((r) => r.form !== false)
       .sort((a, b) => {
@@ -95,7 +96,7 @@ class VisitPage extends Component {
       <svg className="caret-icon" x="0px" y="0px" width="11.848px" height="6.338px" viewBox="351.584 2118.292 11.848 6.338">
         <g><path d="M363.311,2118.414c-0.164-0.163-0.429-0.163-0.592,0l-5.205,5.216l-5.215-5.216c-0.163-0.163-0.429-0.163-0.592,0s-0.163,0.429,0,0.592l5.501,5.501c0.082,0.082,0.184,0.123,0.296,0.123c0.103,0,0.215-0.041,0.296-0.123l5.501-5.501C363.474,2118.843,363.474,2118.577,363.311,2118.414L363.311,2118.414z"/></g>
       </svg>
-    );
+    )
 
     return (
       <section id="visit" className="section-padding">
@@ -111,6 +112,7 @@ class VisitPage extends Component {
                 <DatePicker
                   selected={when}
                   minDate={moment('2018-02-01')}
+                  customInput={<DatePickerButton />}
                   withPortal
                   onChange={this.handleWhenChange}
                 />
@@ -135,6 +137,18 @@ class VisitPage extends Component {
           </div>
         </div>
       </section>
+    )
+  }
+}
+
+class DatePickerButton extends Component {
+  render() {
+    return (
+      <button
+        className="datepicker-button"
+        onClick={this.props.onClick}>
+        {this.props.value}
+      </button>
     )
   }
 }
