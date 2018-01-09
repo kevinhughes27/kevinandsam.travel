90f396e57f329b9959b5a0eaa7f9ea68d6682ced
Kevin Hughes <kevinhughes27@gmail.com>
2018-01-08 23:02:10 -0500

    add script to calc days per country from the route json

---
+++ bin/durations.py
@@ -0,0 +1,30 @@
+# /bin/usr/python
+
+import demjson
+from datetime import datetime
+
+file = open("src/data/route.js")
+javascript_code = file.read()
+
+target = "export const locations ="
+start = javascript_code.find(target) + len(target)
+locations_json = javascript_code[start:]
+
+allLocations = demjson.decode(locations_json)
+locations = [l for l in allLocations if l.get('form') != False]
+
+last_location = locations[0]
+
+for location in locations[1:]:
+
+    start = datetime.strptime(location['date'], '%Y-%m-%d')
+    end = datetime.strptime(last_location['date'], '%Y-%m-%d')
+    duration = start - end
+
+    output = last_location['name'] + ' ' + str(duration.days) + ' days'
+    print(output)
+
+    last_location = location
+
+output = 'Vietnam 18 days'
+print(output)
