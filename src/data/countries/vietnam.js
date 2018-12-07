295c63e1534aa6ff33947c784c81d9ca581f1fa4
Kevin Hughes <kevinhughes27@gmail.com>
2018-12-07 19:58:35 +0700

    update vietnam

--- src/data/countries/vietnam.js
+++ src/data/countries/vietnam.js
@@ -1,28 +1,33 @@
 export default [
   {
     name: "Hanoi, Vietnam",
-    date: "2018-12-04",
+    date: "2018-12-03",
     airport: "HAN",
     coordinates: [21.0227788,105.8194541]
   },
   {
+    name: "Ninh Binh, Vietnam",
+    date: "2018-12-06",
+    coordinates: [20.2451965,105.9404321]
+  },
+  {
     name: "Phong Nha, Vietnam",
-    date: "2018-12-08",
+    date: "2018-12-10",
     coordinates: [17.5862435,106.2810367]
   },
   {
-    name: "Nha Trang, Vietnam",
-    date: "2018-12-11",
-    coordinates: [12.259629,109.1764621]
+    name: "Hue, Vietnam",
+    date: "2018-12-13",
+    coordinates: [16.4534699,107.5420938]
   },
   {
-    name: "Dalat, Vietnam",
-    date: "2018-12-14",
-    coordinates: [11.9040702,108.3806828]
+    name: "Hoi An, Vietnam",
+    date: "2018-12-16",
+    coordinates: [15.9184459,108.3470331]
   },
   {
     name: "Ho Chi Minh, Vietnam",
-    date: "2018-12-18",
+    date: "2018-12-19",
     airport: "SGN",
     coordinates: [10.7553411,106.4150414]
   }
