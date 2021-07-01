fd9236d9620fb57bf65d5d7ca3c9f4a80a58aa55
Kevin Hughes <kevinhughes27@gmail.com>
2021-07-01 15:52:43 -0400

    fix home page load issue

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,45 +7,57 @@ const subheading = "See the World"
 
 class Home extends Component {
   render() {
+    const isServer = (typeof window === 'undefined');
     const mediumScreen = this.props.windowWidth <= 800;
 
-    return (
-      <Layout>
-        { mediumScreen ? renderMobile() : renderDesktop() }
-      </Layout>
-    );
+    if (isServer) {
+      return renderServer()
+    } else if (mediumScreen) {
+      return renderMobile()
+    } else {
+      return renderDesktop()
+    }
   }
 }
 
+const renderServer = () => (
+  <Layout>
+  </Layout>
+)
+
 const renderMobile = () => (
-  <section className='mast'>
-    <header className='mast__header' >
-      <h1>{title}</h1>
-      <span>{subheading}</span>
-    </header>
-    <div className='mast__img'>
-      <div className='absolute-bg' style={{
-          backgroundPosition: '65%',
-          backgroundImage: `url('${__PATH_PREFIX__}/home.jpg')`
-        }}>
+  <Layout>
+    <section className='mast'>
+      <header className='mast__header' >
+        <h1>{title}</h1>
+        <span>{subheading}</span>
+      </header>
+      <div className='mast__img'>
+        <div className='absolute-bg' style={{
+            backgroundPosition: '65%',
+            backgroundImage: `url('${__PATH_PREFIX__}/home.jpg')`
+          }}>
+        </div>
       </div>
-    </div>
-  </section>
+    </section>
+  </Layout>
 )
 
 const renderDesktop = () => (
-  <section className='mast' style={{display: 'flex'}}>
-    <header className='mast__header'>
-      <h1>{title}</h1>
-      <span>{subheading}</span>
-    </header>
-    <div className='mast__img_left'>
-      <div className='absolute-bg' style={{backgroundImage: `url('${__PATH_PREFIX__}/home-left.jpg')`}}></div>
-    </div>
-    <div className='mast__img_right'>
-      <div className='absolute-bg' style={{backgroundImage: `url('${__PATH_PREFIX__}/home-right.jpg')`}}></div>
-    </div>
-  </section>
+  <Layout>
+    <section className='mast' style={{display: 'flex'}}>
+      <header className='mast__header'>
+        <h1>{title}</h1>
+        <span>{subheading}</span>
+      </header>
+      <div className='mast__img_left'>
+        <div className='absolute-bg' style={{backgroundImage: `url('${__PATH_PREFIX__}/home-left.jpg')`}}></div>
+      </div>
+      <div className='mast__img_right'>
+        <div className='absolute-bg' style={{backgroundImage: `url('${__PATH_PREFIX__}/home-right.jpg')`}}></div>
+      </div>
+    </section>
+  </Layout>
 )
 
 const mapSizesToProps = ({ width }) => ({
