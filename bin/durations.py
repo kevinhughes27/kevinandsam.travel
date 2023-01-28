abb1f8d71ed7bda54b4216da295025ed11bced5c
Kevin Hughes <kevinhughes27@gmail.com>
2023-01-28 14:28:01 -0500

    fix durations script

--- bin/durations.py
+++ bin/durations.py
@@ -16,33 +16,30 @@ data_path = Path(__file__).parent / "../src/data/yearTrip.json"
 with open(data_path, "r") as f:
     locations = json.load(f)
 
+locations.pop(0)  # remove starting ottawa
+
+# sort by date because sometimes I took some creative liberty with the ordering in the json file
+# so the map would draw nicer lines
+locations = sorted(locations, key=lambda location: datetime.strptime(location['date'], '%Y-%m-%d'))
+
+# create a list of countries only
 countries = []
 
 for location in locations:
     country_name = location["name"].split(", ")[1] if ", " in location["name"] else location["name"]
 
-    if country_name not in ["Ottawa", "Calgary"]:
-        country_index = next((index for index, country in enumerate(countries) if country["name"] == country_name), None)
-
-        if country_index is None:
-            countries.append({
-                "name": country_name,
-                "date": location["date"],
-                "airport": location.get("airport", None)
-            })
-        else:
-            # add missing airport if needed. sometimes our first location in
-            # a country didn't have an airport if we arrived some other way
-            if not countries[country_index]["airport"] and "airport" in location:
-                countries[country_index]["airport"] = location["airport"]
+    if len(countries) == 0 or country_name != countries[-1]["name"]:
+        countries.append({
+            "name": country_name,
+            "date": location["date"],
+            "airport": location.get("airport", None)
+        })
 
 if 'all' not in sys.argv:
     locations = countries
-else:
-    locations.pop(0)  # remove ottawa
 
+# print output
 last_location = locations[0]
-
 for location in locations:
     start = datetime.strptime(last_location["date"], '%Y-%m-%d')
     end = datetime.strptime(location["date"], '%Y-%m-%d')
