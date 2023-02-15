47a22467361d75f516f48e4d2cfe495557fc3a61
Kevin Hughes <kevinhughes27@gmail.com>
2023-02-15 13:34:38 -0500

    extract post component

--- src/pages/instagram.js
+++ src/pages/instagram.js
@@ -14,95 +14,13 @@ const initialPostsToShow = 24
 const loadInc = 6
 const storageKey = 'ig-postsToShow'
 
-class Index extends React.Component {
+class Post extends React.Component {
   constructor() {
     super()
-    let postsToShow = initialPostsToShow
-
-    if (typeof window !== `undefined`) {
-      postsToShow = parseInt(sessionStorage.getItem(storageKey)) || initialPostsToShow
-    }
-
-    this.state = {
-      postsToShow,
-      activePost: null
-    }
   }
 
-  update() {
-    const distanceToBottom = document.documentElement.offsetHeight - (window.scrollY + window.innerHeight)
-    if (distanceToBottom < 100) {
-      const postsToShow = this.state.postsToShow + loadInc
-      sessionStorage.setItem(storageKey, postsToShow)
-      this.setState({ postsToShow })
-    }
-    this.ticking = false
-  }
-
-  handleScroll = () => {
-    if (!this.ticking) {
-      this.ticking = true
-      requestAnimationFrame(() => this.update())
-    }
-  }
-
-  componentDidMount() {
-    window.addEventListener(`scroll`, this.handleScroll)
-    mousetrap.bind(`left`, () => this.previousPost())
-    mousetrap.bind(`right`, () => this.nextPost())
-    mousetrap.bind(`space`, () => this.nextPost())
-  }
-
-  componentWillUnmount() {
-    window.removeEventListener(`scroll`, this.handleScroll)
-    mousetrap.unbind(`left`)
-    mousetrap.unbind(`right`)
-    mousetrap.unbind(`space`)
-  }
-
-  onClick(index) {
-    this.setState({activePost: index})
-  }
-
-  modalIsOpened() {
-    return this.state.activePost !== null
-  }
-
-  closeModal() {
-    this.setState({activePost: null})
-  }
-
-  previousPost(event) {
-    if (event) {
-      event.stopPropagation()
-    }
-
-    const prevIndex = Math.max(this.state.activePost - 1, 0)
-    this.setState({ activePost: prevIndex })
-  }
-
-  nextPost(event) {
-    if (event) {
-      event.stopPropagation()
-    }
-
-    const nextIndex = Math.min(this.state.activePost + 1, this.props.data.allInstagramPostsJson.nodes.length)
-
-    // load more posts if necessary
-    let postsToShow = this.state.postsToShow
-    if (nextIndex >= postsToShow) {
-      postsToShow += loadInc
-    }
-
-    this.setState({ postsToShow, activePost: nextIndex })
-  }
-
-  renderPost() {
-    if (this.state.activePost === null) {
-      return null
-    }
-
-    const post = this.props.data.allInstagramPostsJson.nodes[this.state.activePost]
+  render() {
+    const post = this.props.post
     const { author, text, timestamp } = post
     const date = new Date(timestamp*1000).toDateString()
 
@@ -170,8 +88,98 @@ class Index extends React.Component {
       </video>
     )
   }
+}
+
+class Index extends React.Component {
+  constructor() {
+    super()
+    let postsToShow = initialPostsToShow
+
+    if (typeof window !== `undefined`) {
+      postsToShow = parseInt(sessionStorage.getItem(storageKey)) || initialPostsToShow
+    }
+
+    this.state = {
+      postsToShow,
+      activePost: null
+    }
+  }
+
+  update() {
+    const distanceToBottom = document.documentElement.offsetHeight - (window.scrollY + window.innerHeight)
+    if (distanceToBottom < 100) {
+      const postsToShow = this.state.postsToShow + loadInc
+      sessionStorage.setItem(storageKey, postsToShow)
+      this.setState({ postsToShow })
+    }
+    this.ticking = false
+  }
+
+  handleScroll = () => {
+    if (!this.ticking) {
+      this.ticking = true
+      requestAnimationFrame(() => this.update())
+    }
+  }
+
+  componentDidMount() {
+    window.addEventListener(`scroll`, this.handleScroll)
+    mousetrap.bind(`left`, () => this.previousPost())
+    mousetrap.bind(`right`, () => this.nextPost())
+    mousetrap.bind(`space`, () => this.nextPost())
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener(`scroll`, this.handleScroll)
+    mousetrap.unbind(`left`)
+    mousetrap.unbind(`right`)
+    mousetrap.unbind(`space`)
+  }
+
+  onClick(index) {
+    this.setState({activePost: index})
+  }
+
+  modalIsOpened() {
+    return this.state.activePost !== null
+  }
+
+  closeModal() {
+    this.setState({activePost: null})
+  }
+
+  previousPost(event) {
+    if (event) {
+      event.stopPropagation()
+    }
+
+    const activePost = this.state.activePost
+    const prevIndex = Math.max(activePost - 1, 0)
+    this.setState({ activePost: prevIndex })
+  }
+
+  nextPost(event) {
+    if (event) {
+      event.stopPropagation()
+    }
+
+    const activePost = this.state.activePost
+    const posts = this.props.data.allInstagramPostsJson.nodes
+    const nextIndex = Math.min(activePost + 1, posts.length)
+
+    // load more posts if necessary
+    let postsToShow = this.state.postsToShow
+    if (nextIndex >= postsToShow) {
+      postsToShow += loadInc
+    }
+
+    this.setState({ postsToShow, activePost: nextIndex })
+  }
 
   renderModal() {
+    const activePost = this.state.activePost
+    const posts = this.props.data.allInstagramPostsJson.nodes
+
     return (
       <Modal
         isOpen={this.modalIsOpened()}
@@ -180,7 +188,7 @@ class Index extends React.Component {
         <FontAwesomeIcon icon={faCaretLeft} className='ig-modal-caret'
           onClick={(e) => this.previousPost(e)}
         />
-        {this.renderPost()}
+        { activePost !== null ? <Post post={(posts[activePost])}/> : null }
         <FontAwesomeIcon icon={faCaretRight} className='ig-modal-caret'
           onClick={(e) => this.nextPost(e)}
         />
